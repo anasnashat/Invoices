85e4662716b3a59ede3d47df01bce8e4e21e4278
anas nashat <aanasht@gmail.com>
2024-03-25 23:24:57 +0200

    add the routs  needed

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesAttachmentController;
 use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SectionController;
+use App\Models\InvoicesAttachment;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
@@ -25,9 +27,12 @@ Route::middleware('auth')->group(function () {
 Route::get('admin/{page}',[AdminController::class,'index']);
 
 Route::resource('invoices', InvoicesController::class);
+Route::resource('attachment', InvoicesAttachmentController::class);
 Route::resource('sections', SectionController::class);
 Route::resource('products', ProductController::class);
 
+
 Route::get('section/{id}',[SectionController::class,'getProducts']);
+Route::get('/download/{file_folder}/{filename}', [InvoicesAttachmentController::class, 'download'])->name('file.download');
 
 require __DIR__.'/auth.php';
