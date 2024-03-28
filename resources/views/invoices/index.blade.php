4ae7af8d6756a3121dbbc3f6f1ccdea690bbcd6a
anas nashat <aanasht@gmail.com>
2024-03-29 01:49:39 +0200

    update

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -19,6 +19,7 @@
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
+                @if(request()->get('filter'))
                 @if(request()->get('filter')['status'] == 1)
                 <h4 class="content-title mb-0 my-auto">الاعدادات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ قائمه الفواتير المدفوعه</span>
                 @elseif(request()->get('filter')['status'] == 0)
@@ -29,6 +30,7 @@
                 <h4 class="content-title mb-0 my-auto">الاعدادات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ قائمه المدفوعه جزئبا</span>
                     @else
                 <h4 class="content-title mb-0 my-auto">الاعدادات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ قائمه الفواتير</span>
+                @endif
                 @endif
 
             </div>
