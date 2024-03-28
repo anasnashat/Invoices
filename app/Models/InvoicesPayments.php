f183d53ef49a58a7de3d1877600c072262879b54
anas nashat <aanasht@gmail.com>
2024-03-29 00:40:05 +0200

    update

--- app/Models/InvoicesPayments.php
+++ app/Models/InvoicesPayments.php
@@ -8,9 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class InvoicesPayments extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'invoice_id',
+        'user_id',
+        'payment_amount',
+        'difference',
+        'note'
+    ];
+
 
     public function invoice()
     {
         return $this->belongsTo(Invoices::class, 'invoice_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
