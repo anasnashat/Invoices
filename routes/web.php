c08fb1963e8d2b4a1b3f403812ca59241ab6e5e6
anas nashat <aanasht@gmail.com>
2024-04-05 00:53:25 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InvoicesAttachmentController;
 use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\InvoicesPaymentsController;
@@ -18,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::fallback(function () {
     return redirect()->route('dashboard');
 });
-Route::get('/dashboard', function () {
-    return view('home.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard',[HomeController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
