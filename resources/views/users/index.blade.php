1cc0aaaff5bdb0ae06465e00d0aa03de26dbdac6
anas nashat <aanasht@gmail.com>
2024-04-11 08:27:01 +0200

    add some of the comments to make the code more readable and good for edite

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,7 +2,7 @@
 @section('css')
 
     @section('title')
-        المستخدمين - مورا سوفت للادارة الفواتير
+        المستخدمين - انس للادارة الفواتير
     @stop
 
     <!-- Internal Data table css -->
